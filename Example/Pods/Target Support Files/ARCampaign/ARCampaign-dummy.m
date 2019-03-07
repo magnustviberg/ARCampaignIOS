abcc7cda7b7afb23512cb868c37f70be2e8f2e2b
Magnus Tviberg <hei@purpl.no>
2019-03-08 00:13:27 +0100

    Initial commit

---
+++ Example/Pods/Target Support Files/ARCampaign/ARCampaign-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ARCampaign : NSObject
+@end
+@implementation PodsDummy_ARCampaign
+@end
---
+++ Example/Pods/Target Support Files/Pods-ARCampaign_Tests/Pods-ARCampaign_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARCampaign_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ARCampaign_Tests
+@end
